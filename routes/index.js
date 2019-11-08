034a956636e1009b6f9a8c1811121076db893ce8
tomni <tom.nicholson1000@gmail.com>
2019-11-08 10:43:02 +0700

    added cart

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var csrf = require('csurf')
-var passport = require('passport')
+var Cart = require('../models/cart')
 
 var Product = require('../models/product')
 
-var csrfProtection = csrf()
-router.use(csrfProtection)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,30 +18,20 @@ router.get('/', function(req, res, next) {
   
 });
 
-router.get('/user/signup', function(req, res, next) {
-  var messages = req.flash('error')
-  res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0})
-});
+router.get('/add-to-cart/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {})
 
-router.post('/user/signup', passport.authenticate('local.signup', {
-  successRedirect: '/user/profile',
-  failureRedirect: '/user/signup',
-  failureFlash: true
-}));
+  Product.findById(productId, function(err, product) {
+    if (err) {
+      res.redirect('/');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart)
+    res.redirect('/')
 
-router.get('/user/profile', function(req, res, next) {
-  res.render('user/profile')
-});
-
-router.get('/user/signin', function(req, res, next) {
-  var messages = req.flash('error')
-  res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0})
-});
-
-router.post('/user/signin', passport.authenticate('local.signin', {
-  successRedirect: '/user/profile',
-  failureRedirect: '/user/signin',
-  failureFlash: true
-}))
+  })
+})
 
 module.exports = router;
