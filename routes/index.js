06406a08c1b5bdd2ef6c247254d0d4d48bb44373
tomni <tom.nicholson1000@gmail.com>
2019-11-11 09:14:36 +0700

    added stripe

--- routes/index.js
+++ routes/index.js
@@ -32,6 +32,23 @@ router.get('/add-to-cart/:id', function(req, res, next) {
     res.redirect('/')
 
   })
+});
+
+router.get('/shopping-cart/', function(req, res, next) {
+  if (!req.session.cart) {
+    res.render('shop/shopping-cart/', {products: null});
+  }
+
+  var cart = new Cart(req.session.cart);
+  res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice})
+})
+
+router.get('/checkout/', function(req, res, next) {
+  if (!req.session.cart) {
+    res.redirect('/shopping-cart');
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('shop/checkout', {total: cart.totalPrice})
 })
 
 module.exports = router;
