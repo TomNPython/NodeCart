861e30fb0212806024fb82d677ca09d0d654519f
tomni <tom.nicholson1000@gmail.com>
2019-11-12 13:32:32 +0700

    reduce and remove added

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 (WIP) Practice building a shopping cart with NodeJS
 
 Part of my learning to use NodeJS and MongoDB with Academind's shopping cart course.
+
+To Do After Finishing:
+
+- need to update stripe checkout from v2 to v3 using emigration docs https://stripe.com/docs/stripe-js/elements/migrating
+- update deprectaed bootstrap hidden classes
+- remove items from db where images aren't working
--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,24 @@ var express = require('express');
 var router = express.Router();
 var Cart = require('../models/cart')
 
-var Product = require('../models/product')
+var Product = require('../models/product');
+var Order = require('../models/order')
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var successMsg = req.flash('success')[0];
   Product.find(function(err, docs) {
     var productChunks = [];
     var chunkSize = 3
     for (let i = 0; i < docs.length; i += chunkSize) {
       productChunks.push(docs.slice(i, i + chunkSize))
     }
-    res.render('shop/index', { title: 'Express', products: productChunks });
+    res.render('shop/index', { 
+      title: 'Express', 
+      products: productChunks, 
+      successMsg: successMsg,
+      noMessage: !successMsg });
   })
   
 });
@@ -30,25 +36,89 @@ router.get('/add-to-cart/:id', function(req, res, next) {
     req.session.cart = cart;
     console.log(req.session.cart)
     res.redirect('/')
-
   })
 });
 
-router.get('/shopping-cart/', function(req, res, next) {
+router.get('/reduce/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {})
+  cart.reduce(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart')
+})
+
+router.get('/remove/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {})
+  cart.remove(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart')
+})
+
+router.get('/shopping-cart', function(req, res, next) {
   if (!req.session.cart) {
-    res.render('shop/shopping-cart/', {products: null});
+    return res.render('shop/shopping-cart', {products: null});
   }
+ 
 
   var cart = new Cart(req.session.cart);
   res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice})
 })
 
-router.get('/checkout/', function(req, res, next) {
+router.get('/checkout', isLoggedIn, function(req, res, next) {
   if (!req.session.cart) {
-    res.redirect('/shopping-cart');
+    return res.redirect('/shopping-cart');
   }
+
   var cart = new Cart(req.session.cart);
-  res.render('shop/checkout', {total: cart.totalPrice})
+  var errMsg = req.flash('error')[0];
+  res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noErrors: !errMsg})
 })
+
+router.post('/checkout', isLoggedIn, function(req, res, next) {
+  if (!req.session.cart) {
+    return res.redirect('/shopping-cart');
+  }
+
+  var cart = new Cart(req.session.cart);
+
+  var stripe = require('stripe')('sk_test_2hO8aNe7emiGk85BeojVTEXc00dXS5Xiju');
+
+  stripe.charges.create(
+    {
+      amount: cart.totalPrice * 100,
+      currency: 'usd',
+      source: req.body.stripeToken,
+      description: 'Charge for jenny.rosen@example.com',
+    },
+    function(err, charge) {
+      // asynchronously called
+      if (err) {
+        req.flash('error', err.message)
+        return res.redirect('/checkout')
+      }
+      var order = new Order({
+        user: req.user,
+        cart: cart,
+        address: req.body.address,
+        name: req.body.name,
+        paymentId: charge.id
+      });
+      order.save(function(err, result) {
+        req.flash('success', 'Payment successful!');
+        req.session.cart = null;
+        res.redirect('/')
+      })
+    }
+);
+})
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+      return next()
+  }
+  req.session.oldUrl = req.url
+  res.redirect('/user/signin')
+}
 
 module.exports = router;
